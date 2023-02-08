3c4519c06ee0fb2e6aea21ac384065e01b75f148
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2023-02-08 09:22:12 -0500

    fixed fetch req

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,14 +2,12 @@ const sendData = async (event) => {
     event.preventDefault()
     let blogForm = document.getElementById("blog-form")
     let formData = new FormData(blogForm)
-    formData.forEach((value, key) => {
-        console.log(value,key)
-    })
-
+    let data = Object.fromEntries(formData)
+    
     const response = await fetch("/api/data/create-blog", {
         method: "POST",
-        // headers: { "Content-Type": "application/json"},
-        body: formData
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(data)
     });
     switch (response.status) {
         case 200:
